b53640fd2cd48ed4a439aab3ee79fd7886b9ba79
Facundo Ferrari <88807740+Facu-Ferra@users.noreply.github.com>
2024-04-24 22:36:54 -0300

    Pruebas en pantalla de inicio (todo en app.js)

--- wealth-quest/App.js
+++ wealth-quest/App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import tabern from './assets/images/tabern.png';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <ImageBackground source={tabern} style={styles.backImage}>
+          <Text style={styles.text}>¿Ready to keep growing?</Text>
+          <TouchableOpacity onPress={onPress}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Start</Text>
+              </View>
+          </TouchableOpacity>
+       </ImageBackground>
     </View>
   );
 }
 
+function onPress(){}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
+  backImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-end'
+  },
+  text: {
+    color: 'white',
+    fontSize: 18,
+    marginBottom: 5,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button: {
+    width: '70%',
+    maxWidth: 400,
+    marginBottom:85,
+    height: 60,
+    borderRadius: 40,
+    backgroundColor: '#97111D',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center', // Centrar horizontalmente el botón
+    borderWidth: 2, // Grosor del borde
+    borderColor: 'black', // Color del borde
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+  }
 });
