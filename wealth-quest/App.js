5a9eec7695569f608f35fdf17f0de3fb80210bde
Facundo Ferrari <88807740+Facu-Ferra@users.noreply.github.com>
2024-04-25 01:36:45 -0300

    feat: se agrega fuente InknutAntiqua

--- wealth-quest/App.js
+++ wealth-quest/App.js
@@ -1,23 +1,51 @@
 import { ImageBackground, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import tabern from './assets/images/tabern.png';
 import btn from './assets/images/btn1_start.png';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  InknutAntiqua_300Light,
+  InknutAntiqua_400Regular,
+  InknutAntiqua_500Medium,
+  InknutAntiqua_600SemiBold,
+  InknutAntiqua_700Bold,
+  InknutAntiqua_800ExtraBold,
+  InknutAntiqua_900Black,
+} from '@expo-google-fonts/inknut-antiqua';
 export default function App() {
-  return (
-    <View style={styles.container}>
-       <ImageBackground source={tabern} style={styles.backImage}>
+  let [fontsLoaded] = useFonts({
+    InknutAntiqua_300Light,
+    InknutAntiqua_400Regular,
+    InknutAntiqua_500Medium,
+    InknutAntiqua_600SemiBold,
+    InknutAntiqua_700Bold,
+    InknutAntiqua_800ExtraBold,
+    InknutAntiqua_900Black,
+  });
+
+  let fontSize = 24;
+  let paddingVertical = 6;
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={tabern} style={styles.backImage}>
           <Text style={styles.text}>¿Ready to keep growing?</Text>
           <TouchableOpacity onPress={onPress}>
             <ImageBackground source={btn} style={styles.button}>
-                <Text style={styles.buttonText}>Start</Text>
+              <Text style={styles.buttonText}>Start</Text>
             </ImageBackground>
           </TouchableOpacity>
-       </ImageBackground>
-    </View>
-  );
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 
-function onPress(){}
+function onPress() { }
 
 const styles = StyleSheet.create({
   container: {
@@ -33,23 +61,23 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 18,
     marginBottom: 5,
-    textAlign: 'center'
+    textAlign: 'center',
+    fontFamily: 'InknutAntiqua_300Light',
   },
   button: {
     width: 320,
-    marginBottom:75,
+    marginBottom: 75,
     height: 60,
     borderRadius: 40,
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center', // Centrar horizontalmente el botón
-    borderWidth: 1, // Grosor del borde
-    borderColor: 'black', // Color del borde
   },
   buttonText: {
     color: 'white',
     fontSize: 18,
     textAlign: 'center',
-    fontWeight: 'bold',
-  }
+    fontFamily: 'InknutAntiqua_600SemiBold',
+  },
 });
+
